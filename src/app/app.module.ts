7d9519e1cca1282923e349bba0885450d32136f1
jzhang <jzepitech@gmail.com>
2019-01-13 18:17:42 +0100

    Header done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
+/* COMPONENTS */
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { InformationBarComponent } from './components/information-bar/information-bar.component';
+
+/* CONTAINERS */
 import { HomeComponent } from './containers/home/home.component';
+import { HeaderComponent } from './containers/layout/header/header.component';
+import { FooterComponent } from './containers/layout/footer/footer.component';
+
 
 @NgModule({
   declarations: [
+    /* COMPONENTS */
     AppComponent,
-    HomeComponent
+    MenuComponent,
+    NavbarComponent,
+    CarouselComponent,
+    InformationBarComponent,
+
+    /* CONTAINERS */
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/information-bar/information-bar.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-information-bar',
+  templateUrl: './information-bar.component.html',
+  styleUrls: ['./information-bar.component.css']
+})
+export class InformationBarComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+
+}
